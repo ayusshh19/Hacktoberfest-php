9113bc154b42a2c8d29b50d076e902eb3078e020
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2023-10-16 08:37:20 +0530

    changed dbname to hacktoberfest

--- signup.php
+++ signup.php
@@ -5,7 +5,9 @@ if(isset($_POST['signupform'])){
     $username1=$_POST["username"];
     $new_sql="SELECT * FROM `signup` WHERE username='$username1'";
     $result1=mysqli_query($conn,$new_sql);
-    if(mysqli_num_rows($result1)>0){
+    if ($result1 === false) {
+        echo "Query Error: " . mysqli_error($conn);
+    } else if (mysqli_num_rows($result1)>0){
         echo "username already exist!!!!";
     }
     else{
@@ -15,7 +17,7 @@ if(isset($_POST['signupform'])){
     $tempname = $_FILES["fileprofile"]["tmp_name"];    
         $folder = "image/".$filename;   
     $new_pass1=sha1($pass1);
-    $sql="INSERT INTO `signup` (`srno`, `username`, `email`, `pass`,`file`) VALUES (NULL, '$username1', '$email', '$new_pass1','$folder')";
+    $sql="INSERT INTO `signup` ( `username`, `email`, `pass`,`file`) VALUES ( '$username1', '$email', '$new_pass1','$folder')";
     $result=mysqli_query($conn,$sql);
     if ($result) {
         if (move_uploaded_file($tempname, $folder))  {
